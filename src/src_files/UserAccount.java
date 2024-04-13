4348d88227774bf336929d4fde215cc0e4b8434c
Nirali Rathod <nirali.h.rathod@gmail.com>
2024-04-13 15:45:04 -0500

    method changes

--- src/src_files/UserAccount.java
+++ src/src_files/UserAccount.java
@@ -127,36 +127,7 @@ public class UserAccount {
 
 		}
 	}
-
-	public void recommendationSection() {
-		String selection = "";
-
-		while (!selection.equals("x")) {
-			System.out.println("-----------------------------");
-			System.out.println("Please Select Option from Below: ");
-			System.out.println("-----------------------------");
-			System.out.println("1: Send Recommendation Request to Connected People ");
-			System.out.println("2: View Your Recommendations ");
-			System.out.println("x: Exit");
-
-			selection = input.nextLine();
-			System.out.println();
-
-			switch (selection) {
-			case "1":
-				requestRec(loginID);
-				continue;
-			case "2":
-				showRec(loginID);
-				continue;
-			case "x":
-				break;
-			default:
-				System.out.println("Option invalid !!!");
-			}
-		}
-	}
-
+	
 	public void connectionSection() {
 		String selection = "";
 
@@ -190,6 +161,35 @@ public class UserAccount {
 		}
 	}
 
+	public void recommendationSection() {
+		String selection = "";
+
+		while (!selection.equals("x")) {
+			System.out.println("-----------------------------");
+			System.out.println("Please Select Option from Below: ");
+			System.out.println("-----------------------------");
+			System.out.println("1: Send Recommendation Request to Connected People ");
+			System.out.println("2: View Your Recommendations ");
+			System.out.println("x: Exit");
+
+			selection = input.nextLine();
+			System.out.println();
+
+			switch (selection) {
+			case "1":
+				requestRec(loginID);
+				continue;
+			case "2":
+				showRec(loginID);
+				continue;
+			case "x":
+				break;
+			default:
+				System.out.println("Option invalid !!!");
+			}
+		}
+	}
+	
 	public void jobsSection() {
 		String selection = "";
 
@@ -253,94 +253,8 @@ public class UserAccount {
 		}
 
 	}
-
-	public void showSharedJobs(String loginID) {
-
-		ArrayList<Jobs> sharedJob = data.viewSharedJobs(loginID);
-
-		if (!sharedJob.isEmpty()) {
-			System.out.println("--- Jobs Shared By You ---");
-
-			for (Jobs j : sharedJob) {
-				System.out.println("-----------------------------");
-				System.out.println(
-						" Job Posted by : " + data.getUserFullName(j.getCreator()) + " On " + j.getDateandtime());
-				System.out.println(" Job Title : " + j.getJobTitle());
-				System.out.println(" Job Description : " + j.getJobDesc());
-				System.out.println("-----------------------------");
-			}
-		} else {
-			System.out.println("--- No Jobs Shared By You  ---");
-		}
-
-	}
-
-	public void showPostedJobs(String loginID) {
-
-		ArrayList<Jobs> postedJobs = data.viewPostedJobs(loginID);
-
-		if (!postedJobs.isEmpty()) {
-			System.out.println("--- Jobs Posted By " + loginID + " ---");
-
-			for (Jobs j : postedJobs) {
-				System.out.println("-----------------------------");
-				System.out.println(" Job Posted On " + j.getDateandtime());
-				System.out.println(" Job Title : " + j.getJobTitle());
-				System.out.println(" Job Description : " + j.getJobDesc());
-				System.out.println("-----------------------------");
-			}
-		} else {
-			System.out.println("--- No Jobs Posted By You  ---");
-		}
-
-	}
-
-	public void showRec(String loginID) {
-
-		ArrayList<Recommendation> recs = data.recommendations(loginID);
-
-		if (!recs.isEmpty()) {
-
-			System.out.println("--- Recommendations  ---");
-			for (Recommendation r : recs) {
-
-				if (r.getSenderId().equals(loginID))
-					System.out.println("You have been recommended by " + data.getUserFullName(r.getReceiverId()));
-				else if (r.getReceiverId().equals(loginID))
-					System.out.println("You recommended " + data.getUserFullName(r.getSenderId()));
-				// printUserProfile(r.getLoginID());
-			}
-		} else {
-			System.out.println("--- No Recommendations  ---");
-		}
-
-	}
-
-	public void searchUser() {
-
-		String selection = "";
-		System.out.print("Search :");
-		selection = input.nextLine();
-
-		ArrayList<UserAccount> searches = data.searchUser(selection, getLoginID());
-
-		if (!searches.isEmpty()) {
-			for (UserAccount ua : searches) {
-				printUserAccount(ua);
-
-				System.out.println(" Enter 'y' to send connection request : ");
-				selection = input.nextLine();
-
-				if (selection.equals("y")) {
-					data.sendRequestForConnection(getLoginID(), ua.getLoginID());
-				} else
-					continue;
-			}
-		} else {
-			System.out.println("--- No Search Result Found ---");
-		}
-	}
-
+	
+	
 	public void showNotifications(String loginID) {
 
 		String selection = "";
@@ -387,73 +301,8 @@ public class UserAccount {
 		System.out.println("");
 	}
 
-	public void postJob(String loginID) {
-
-		System.out.println("Enter Job Title");
-		String title = input.next();
-
-		System.out.println("Enter Job Description");
-		String desc = input.next();
-
-		data.PostJob(loginID, title, desc);
-
-	}
-
-	public void showJobs(String loginID) {
-
-		String selection = "";
-
-		ArrayList<Jobs> jobsByRec = data.ViewJobsbyRecruiter(loginID);
-
-		if (!jobsByRec.isEmpty()) {
-			System.out.println("--- New Jobs Posted By Recruiters ---");
-			System.out.println("--- Enter 'y' to share job to your connections  ---");
-
-			for (Jobs j : jobsByRec) {
-
-				printJob(j);
-
-				selection = input.nextLine();
-
-				if (selection.equals("y")) {
-					data.ShareJob(loginID, j.getJobID());
-
-				} else
-					continue;
-			}
-		} else {
-			System.out.println("--- No New Jobs Posted By Recruiters  ---");
-		}
-
-	}
-
-	public void showConnections(String loginid) {
-
-		String selection = "";
-
-		ArrayList<UserAccount> userConn = data.getConnections(loginid);
-
-		if (!userConn.isEmpty()) {
-			System.out.println("--- Connection List for " + getFirstName() + " " + getLastName() + " ---");
-			System.out.println("--- Enter 'v' to view connection profile  ---");
-
-			for (UserAccount u : userConn) {
-
-				printUserAccount(u);
-
-				selection = input.nextLine();
-
-				if (selection.equals("v")) {
-					printUserProfile(u.getLoginID());
-
-				} else
-					continue;
-			}
-		} else {
-			System.out.println("--- No Connection List Available For " + getFirstName() + " " + getLastName() + " ---");
-		}
-	}
-
+	
+	
 	public void requestRec(String loginid) {
 
 		String selection = "";
@@ -483,6 +332,87 @@ public class UserAccount {
 		}
 	}
 
+	
+
+	public void showIncomingRecRequests(String loginId) {
+		String selection = "";
+		ArrayList<UserAccount> Recreq = data.viewIncomingRecRequests(loginId);
+
+		if (!Recreq.isEmpty()) {
+
+			System.out.println("\n ------ Incoming Recommendation Requests ------ ");
+			System.out.println(
+					"\n --- Enter 'y' to accept connection request, 'n' to reject or press enter for next : --- \n");
+
+			for (UserAccount u : Recreq) {
+
+				printUserAccount(u);
+
+				selection = input.nextLine();
+
+				if (selection.equals("y")) {
+					data.updateRecommendation(loginId, u.getLoginID(), "Approved");
+				} else if (selection.equals("n")) {
+					data.updateRecommendation(loginId, u.getLoginID(), "Rejected");
+				} else
+					continue;
+
+			}
+		} else {
+			System.out.println("\n --- No Incoming Recommendation Request Available ---");
+		}
+	}
+
+	public void showRec(String loginID) {
+
+		ArrayList<Recommendation> recs = data.recommendations(loginID);
+
+		if (!recs.isEmpty()) {
+
+			System.out.println("--- Recommendations  ---");
+			for (Recommendation r : recs) {
+
+				if (r.getSenderId().equals(loginID))
+					System.out.println("You have been recommended by " + data.getUserFullName(r.getReceiverId()));
+				else if (r.getReceiverId().equals(loginID))
+					System.out.println("You recommended " + data.getUserFullName(r.getSenderId()));
+				// printUserProfile(r.getLoginID());
+			}
+		} else {
+			System.out.println("--- No Recommendations  ---");
+		}
+
+	}
+
+
+	
+	public void showConnections(String loginid) {
+
+		String selection = "";
+
+		ArrayList<UserAccount> userConn = data.getConnections(loginid);
+
+		if (!userConn.isEmpty()) {
+			System.out.println("--- Connection List for " + getFirstName() + " " + getLastName() + " ---");
+			System.out.println("--- Enter 'v' to view connection profile  ---");
+
+			for (UserAccount u : userConn) {
+
+				printUserAccount(u);
+
+				selection = input.nextLine();
+
+				if (selection.equals("v")) {
+					printUserProfile(u.getLoginID());
+
+				} else
+					continue;
+			}
+		} else {
+			System.out.println("--- No Connection List Available For " + getFirstName() + " " + getLastName() + " ---");
+		}
+	}
+	
 	public void showConnectionRecommendations(String loginid) {
 
 		String selection = "";
@@ -564,75 +494,7 @@ public class UserAccount {
 			System.out.println("\n --- No Incoming Connection Request Available ---");
 		}
 	}
-
-	public void showIncomingRecRequests(String loginId) {
-		String selection = "";
-		ArrayList<UserAccount> Recreq = data.viewIncomingRecRequests(loginId);
-
-		if (!Recreq.isEmpty()) {
-
-			System.out.println("\n ------ Incoming Recommendation Requests ------ ");
-			System.out.println(
-					"\n --- Enter 'y' to accept connection request, 'n' to reject or press enter for next : --- \n");
-
-			for (UserAccount u : Recreq) {
-
-				printUserAccount(u);
-
-				selection = input.nextLine();
-
-				if (selection.equals("y")) {
-					data.updateRecommendation(loginId, u.getLoginID(), "Approved");
-				} else if (selection.equals("n")) {
-					data.updateRecommendation(loginId, u.getLoginID(), "Rejected");
-				} else
-					continue;
-
-			}
-		} else {
-			System.out.println("\n --- No Incoming Recommendation Request Available ---");
-		}
-	}
-
-	public void printJob(Jobs jb) {
-
-		System.out.println("-----------------------------");
-		System.out.println(" Job Posted by : " + data.getUserFullName(jb.getCreator()) + " On " + jb.getDateandtime());
-		System.out.println(" Job Title : " + jb.getJobTitle());
-		System.out.println(" Job Description : " + jb.getJobDesc());
-		System.out.println("-----------------------------");
-		System.out.print(" Do you want to share it  ? ");
-		System.out.println("");
-
-	}
-
-	public void printUserAccount(UserAccount ua) {
-		if (ua.getType().equals("Regular")) {
-			System.out.print(ua.getFirstName() + " " + ua.getLastName() + " at " + ua.getCompany() + ": ");
-		} else
-			System.out.print(ua.getFirstName() + " " + ua.getLastName() + " - " + ua.getType() + " at "
-					+ ua.getCompany() + ": ");
-	}
-
-	public void printUserProfile(String profileID) {
-		UserAccount conProf = data.viewConnectionProfile(profileID);
-
-		if (conProf != null) {
-
-			System.out.println("-------------------------------------------------------");
-			System.out.println(data.getUserFullName(profileID) + "'s Profle");
-			System.out.println("Full Name : " + data.getUserFullName(profileID));
-			System.out.println("Company : " + conProf.getCompany());
-			if (conProf.getType().equals("Regular"))
-				System.out.println("Position : Staff");
-			else
-				System.out.println("Position : " + conProf.getType());
-			System.out.println("-------------------------------------------------------");
-
-		} else {
-			System.out.println("--- No profile found !!! ---");
-		}
-	}
+	
 
 	public void showTOP3JobRecommendations(String loginid) {
 
@@ -663,6 +525,158 @@ public class UserAccount {
 
 	}
 
+
+	public void showSharedJobs(String loginID) {
+
+		ArrayList<Jobs> sharedJob = data.viewSharedJobs(loginID);
+
+		if (!sharedJob.isEmpty()) {
+			System.out.println("--- Jobs Shared By You ---");
+
+			for (Jobs j : sharedJob) {
+				System.out.println("-----------------------------");
+				System.out.println(
+						" Job Posted by : " + data.getUserFullName(j.getCreator()) + " On " + j.getDateandtime());
+				System.out.println(" Job Title : " + j.getJobTitle());
+				System.out.println(" Job Description : " + j.getJobDesc());
+				System.out.println("-----------------------------");
+			}
+		} else {
+			System.out.println("--- No Jobs Shared By You  ---");
+		}
+
+	}
+
+	public void showPostedJobs(String loginID) {
+
+		ArrayList<Jobs> postedJobs = data.viewPostedJobs(loginID);
+
+		if (!postedJobs.isEmpty()) {
+			System.out.println("--- Jobs Posted By " + loginID + " ---");
+
+			for (Jobs j : postedJobs) {
+				System.out.println("-----------------------------");
+				System.out.println(" Job Posted On " + j.getDateandtime());
+				System.out.println(" Job Title : " + j.getJobTitle());
+				System.out.println(" Job Description : " + j.getJobDesc());
+				System.out.println("-----------------------------");
+			}
+		} else {
+			System.out.println("--- No Jobs Posted By You  ---");
+		}
+
+	}
+	
+	public void postJob(String loginID) {
+
+		System.out.println("Enter Job Title");
+		String title = input.next();
+
+		System.out.println("Enter Job Description");
+		String desc = input.next();
+
+		data.PostJob(loginID, title, desc);
+
+	}
+
+	public void showJobs(String loginID) {
+
+		String selection = "";
+
+		ArrayList<Jobs> jobsByRec = data.ViewJobsbyRecruiter(loginID);
+
+		if (!jobsByRec.isEmpty()) {
+			System.out.println("--- New Jobs Posted By Recruiters ---");
+			System.out.println("--- Enter 'y' to share job to your connections  ---");
+
+			for (Jobs j : jobsByRec) {
+
+				printJob(j);
+
+				selection = input.nextLine();
+
+				if (selection.equals("y")) {
+					data.ShareJob(loginID, j.getJobID());
+
+				} else
+					continue;
+			}
+		} else {
+			System.out.println("--- No New Jobs Posted By Recruiters  ---");
+		}
+
+	}
+
+	
+	public void printJob(Jobs jb) {
+
+		System.out.println("-----------------------------");
+		System.out.println(" Job Posted by : " + data.getUserFullName(jb.getCreator()) + " On " + jb.getDateandtime());
+		System.out.println(" Job Title : " + jb.getJobTitle());
+		System.out.println(" Job Description : " + jb.getJobDesc());
+		System.out.println("-----------------------------");
+		System.out.print(" Do you want to share it  ? ");
+		System.out.println("");
+
+	}
+	
+	public void searchUser() {
+
+		String selection = "";
+		System.out.print("Search :");
+		selection = input.nextLine();
+
+		ArrayList<UserAccount> searches = data.searchUser(selection, getLoginID());
+
+		if (!searches.isEmpty()) {
+			for (UserAccount ua : searches) {
+				printUserAccount(ua);
+
+				System.out.println(" Enter 'y' to send connection request : ");
+				selection = input.nextLine();
+
+				if (selection.equals("y")) {
+					data.sendRequestForConnection(getLoginID(), ua.getLoginID());
+				} else
+					continue;
+			}
+		} else {
+			System.out.println("--- No Search Result Found ---");
+		}
+	}
+
+	
+
+
+	public void printUserAccount(UserAccount ua) {
+		if (ua.getType().equals("Regular")) {
+			System.out.print(ua.getFirstName() + " " + ua.getLastName() + " at " + ua.getCompany() + ": ");
+		} else
+			System.out.print(ua.getFirstName() + " " + ua.getLastName() + " - " + ua.getType() + " at "
+					+ ua.getCompany() + ": ");
+	}
+
+	public void printUserProfile(String profileID) {
+		UserAccount conProf = data.viewConnectionProfile(profileID);
+
+		if (conProf != null) {
+
+			System.out.println("-------------------------------------------------------");
+			System.out.println(data.getUserFullName(profileID) + "'s Profle");
+			System.out.println("Full Name : " + data.getUserFullName(profileID));
+			System.out.println("Company : " + conProf.getCompany());
+			if (conProf.getType().equals("Regular"))
+				System.out.println("Position : Staff");
+			else
+				System.out.println("Position : " + conProf.getType());
+			System.out.println("-------------------------------------------------------");
+
+		} else {
+			System.out.println("--- No profile found !!! ---");
+		}
+	}
+
+	
 	public String getLoginID() {
 		return loginID;
 	}
